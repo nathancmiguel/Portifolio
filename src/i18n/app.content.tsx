c48db889527efe5333cb30634e7da66a7fe07457
Nathan Miguel <nathan.cm77@gmail.com>
2026-08-23 18:02:23 -0300

    chore: update meta tags and site content to include discloud.dev branding

--- src/i18n/app.content.tsx
+++ src/i18n/app.content.tsx
@@ -5,12 +5,12 @@ const appContent = {
   schema: undefined,
   content: {
     title: t({
-      en: "Nathan Miguel | Software Development",
-      "pt-BR": "Nathan Miguel | Desenvolvimento de Software",
+      en: "Nathan Miguel | Dev | discloud.dev",
+      "pt-BR": "Nathan Miguel | Dev | discloud.dev",
     }),
     description: t({
-      en: "Portfolio of Nathan Miguel, specialized in Software Development, backend and frontend. Check out my projects and dev logs.",
-      "pt-BR": "Portfólio de Nathan Miguel, especializado em Desenvolvimento de Software, backend e frontend. Veja meus projetos e logs.",
+      en: "Portfolio of Nathan Miguel, specialized in Software Development, backend, frontend and discloud.dev. Check out my projects and dev logs.",
+      "pt-BR": "Portfólio de Nathan Miguel, especializado em Desenvolvimento de Software, backend, frontend e discloud.dev. Veja meus projetos e logs.",
     }),
   },
 } satisfies Dictionary;
