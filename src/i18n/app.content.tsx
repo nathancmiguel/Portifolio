66d3e9f7ddd739df5e230f2ea79abcc457e2552e
Nathan Miguel <nathan.cm77@gmail.com>
2026-08-23 18:14:32 -0300

    refactor: remove discloud.dev branding from meta tags and application content

--- src/i18n/app.content.tsx
+++ src/i18n/app.content.tsx
@@ -5,12 +5,12 @@ const appContent = {
   schema: undefined,
   content: {
     title: t({
-      en: "Nathan Miguel | Dev | discloud.dev",
-      "pt-BR": "Nathan Miguel | Dev | discloud.dev",
+      en: "Nathan Miguel | Dev",
+      "pt-BR": "Nathan Miguel | Dev",
     }),
     description: t({
-      en: "Portfolio of Nathan Miguel, specialized in Software Development, backend, frontend and discloud.dev. Check out my projects and dev logs.",
-      "pt-BR": "Portfólio de Nathan Miguel, especializado em Desenvolvimento de Software, backend, frontend e discloud.dev. Veja meus projetos e logs.",
+      en: "Portfolio of Nathan Miguel, specialized in Software Development, backend and frontend. Check out my projects and dev logs.",
+      "pt-BR": "Portfólio de Nathan Miguel, especializado em Desenvolvimento de Software and backend, frontend. Veja meus projetos e logs.",
     }),
   },
 } satisfies Dictionary;
